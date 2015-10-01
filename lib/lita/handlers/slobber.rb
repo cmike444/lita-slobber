6be7df2412cecb70b9c5a42713a97d6d208bee4f
Chris Mikelson <chris.mikelson@640labs.com>
2015-09-30 22:21:39 -0500

    cleaned up, new attempt at channel

--- lib/lita/handlers/slobber.rb
+++ lib/lita/handlers/slobber.rb
@@ -7,31 +7,29 @@ module Lita
       route(/^start taking notes/, :start_taking_notes, help: {
         "bob: start taking notes" => "Starts recording conversation until the `stop taking notes` command is given."
         })
+      route(/^stop taking notes/, :stop_taking_notes, help: {
+        "bob: stop taking notes" => "Stops recording conversation after the `start taking notes` command is given."
+        })
+      route(/^get channel/, :get_channel, command: true, help: {
+        "bob: get channel" => "Replies with CHANNEL"
+        })
 
-      def start_taking_notes(request, response)
+      def start_taking_notes(response)
         start_marker = Time.now
         # unique_id = request.slack_channel
         # redis.set(unique_id, start_marker)
         response.reply "Alright, it's #{start_marker.strftime('%l:%M %P')} and I'm ready to take notes."
       end
 
-      route(/^stop taking notes/, :stop_taking_notes, help: {
-        "bob: stop taking notes" => "Stops recording conversation after the `start taking notes` command is given."
-        })
-
-      def stop_taking_notes(request, response)
+      def stop_taking_notes(response)
         stop_marker = Time.now
         # unique_id = request.slack_channel
         # redis.get(unique_id, stop_marker)
         response.reply  "Ok, it's #{stop_marker.strftime('%l:%M %P')} and I'm done taking notes. I'll have them compiled and sent out in a jiffy!"
       end
 
-      route(/^get channel info/, :get_channel, command: true, help: {
-        "bob: get channel" => "Replies with CHANNEL"
-        })
-
-      def get_channel(request, response)
-        response.reply "#{request.inspect}"
+      def get_channel(response)
+        response.reply "#{response.message.source.room_object}"
       end
 
     end
