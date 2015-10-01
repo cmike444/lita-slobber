1f7b0f3fd597f217e2c4782c8bb000cc08594794
Chris Mikelson <chris.mikelson@640labs.com>
2015-09-30 23:18:08 -0500

    remove time ago in words

--- lib/lita/handlers/slobber.rb
+++ lib/lita/handlers/slobber.rb
@@ -10,9 +10,6 @@ module Lita
       route(/^stop taking notes/, :stop_taking_notes, command: true, help: {
         "stop taking notes" => "Stops recording conversation after the `start taking notes` command is given."
         })
-      route(/^get channel/, :get_channel, command: true, help: {
-        "get channel" => "Replies with CHANNEL"
-        })
 
       def start_taking_notes(response)
         start = Time.now
@@ -25,34 +22,7 @@ module Lita
         stop = Time.now
         channel = get_channel(response)
         start = redis.get(channel.id)
-        response.reply  "Ok, it's #{stop.strftime('%l:%M %P')} and I'm done taking notes. I'll have the notes from the last #{time_ago_in_words(start, stop)} compiled and sent out in a jiffy!"
-      end
-
-      def time_ago_in_words(t1, t2)
-        s = t1.to_i - t2.to_i # distance between t1 and t2 in seconds
-
-        resolution = if s > 29030400 # seconds in a year
-          [(s/29030400), 'years'] 
-        elsif s > 2419200
-          [(s/2419200), 'months']
-        elsif s > 604800
-          [(s/604800), 'weeks']
-        elsif s > 86400
-          [(s/86400), 'days']
-        elsif s > 3600 # seconds in an hour
-          [(s/3600), 'hours'] 
-        elsif s > 60
-          [(s/60), 'minutes']
-        else
-          [s, 'seconds']
-        end
-
-        # singular v. plural resolution
-        if resolution[0] == 1
-          resolution.join(' ')[0...-1]
-        else
-          resolution.join(' ')
-        end
+        response.reply  "Ok, it's #{stop.strftime('%l:%M %P')} and I'm done taking notes. I'll have the notes compiled and sent out in a jiffy!"
       end
 
       def get_channel(response)
