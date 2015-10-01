36d5db6db3c71dd786d34c8e3085109e2a244ad6
Chris Mikelson <chris.mikelson@640labs.com>
2015-09-30 21:44:16 -0500

    updated start and stop

--- lib/lita/handlers/slobber.rb
+++ lib/lita/handlers/slobber.rb
@@ -5,34 +5,33 @@ module Lita
     class Slobber < Handler
 
       route(/^start taking notes/, :start_taking_notes, help: {
-        "start taking notes" => "Starts recording conversation until the `stop taking notes` command is given."
+        "bob: start taking notes" => "Starts recording conversation until the `stop taking notes` command is given."
         })
 
       def start_taking_notes(request, response)
         start_marker = Time.now
-        unique_id = request.slack_channel
-        redis.set(unique_id, start_marker)
-        response.reply "This comes first."
-        response.reply "Alright, it's #{start_marker.strftime('%l:%M %P')} and I'm ready to take notes in #{unique_id}."
+        # unique_id = request.slack_channel
+        # redis.set(unique_id, start_marker)
+        response.reply "Alright, it's #{start_marker.strftime('%l:%M %P')} and I'm ready to take notes."
       end
 
       route(/^stop taking notes/, :stop_taking_notes, help: {
-        "stop taking notes" => "Stops recording conversation after the `start taking notes` command is given."
+        "bob: stop taking notes" => "Stops recording conversation after the `start taking notes` command is given."
         })
 
       def stop_taking_notes(request, response)
         stop_marker = Time.now
-        unique_id = request.slack_channel
-        redis.get(unique_id, stop_marker)
+        # unique_id = request.slack_channel
+        # redis.get(unique_id, stop_marker)
         response.reply  "Ok, it's #{stop_marker.strftime('%l:%M %P')} and I'm done taking notes. I'll have them compiled and sent out in a jiffy!"
       end
 
       route(/^get channel info/, :get_channel, command: true, help: {
-        "get channel" => "Replies with CHANNEL"
+        "bob: get channel" => "Replies with CHANNEL"
         })
 
       def get_channel(request, response)
-        response.reply "#{request.slack_channel.raw_data}"
+        response.reply "#{request.slack_channel.inspect}"
       end
 
     end
