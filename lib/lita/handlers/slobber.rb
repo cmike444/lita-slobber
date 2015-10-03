d99cc01ca662a67301747129645d6d4e75998095
Chris Mikelson <chris.mikelson@640labs.com>
2015-10-03 12:03:18 -0500

    more refactoring

--- lib/lita/handlers/slobber.rb
+++ lib/lita/handlers/slobber.rb
@@ -28,37 +28,19 @@ module Lita
       end
 
       def stop_taking_notes(response)
-        stop = Time.now.to_i
         channel = get_channel(response)
-        
+        username = get_reply_to_name(response)
+
         if is_taking_notes(channel)
-          start = redis.get(channel.id)
           redis.del(channel.id)
-
-          stopped_taking_notes = [
-            "Ok, cool. I've compiled your notes and sent them out!",
-            "Right on, I'll send out the notes in a jiffy!",
-            "Alright, I'll get your notes put together and sent out right away!",
-            "Awesome, you'll see the notes in your email shortly."
-          ].sample
-
-          response.reply  stopped_taking_notes
+          response.reply  stopped_taking_notes_response(username)
         else
-
-          not_taking_notes = [
-            "You haven't told me to `start taking notes` yet...",
-            "You already asked me to take notes...",
-            "Yeah, I've been doing that...",
-            "Uh huh, that's what I'm doing right now..."
-          ].sample
-
-          response.reply not_taking_notes
+          response.reply not_taking_notes_response(username)
         end
       end
 
       def take_notes(response)
         channel = get_channel(response)
-        
         FileUtils.mkdir_p("tmp/#{channel.id}") unless Dir.exists?("tmp/#{channel.id}")
         
         if is_taking_notes(channel)         
@@ -69,32 +51,6 @@ module Lita
           File.open("tmp/#{channel.id}/notes_session.log", 'w')
           return
         end
-      end
-
-      def started_taking_notes_response(username)
-        started_taking_notes = [
-            "Alright #{username}, I'm ready to take notes.",
-            "Ok, sounds good. Ready when you are, #{username}!",
-            "I'm already on top of it, #{username}!",
-            "Alright, well then start typing #{username}!",
-            "For shizzle #{username}.",
-            "Uh.. Yeah. That's my job, #{username}.",
-            "Alright, #{username}. I've got you covered!",
-            "You bet, #{username}.",
-            "That I can do, #{username}.",
-            "If you say so, #{username}."
-          ].sample
-          started_taking_notes
-      end
-
-      def already_taking_notes_response(username)
-        already_taking_notes = [
-            "I've been taking notes, #{username}...",
-            "You already asked me to take notes #{username}...",
-            "Yeah #{username}, I've been doing that...",
-            "Uh huh, that's what I'm doing #{username}..."
-          ].sample
-          already_taking_notes
       end
 
       def get_channel(response)
@@ -109,8 +65,51 @@ module Lita
         redis.get(channel.id) ? true : false
       end
 
-      def is_private_message(response)
-        response.message.source.private_message
+      def started_taking_notes_response(username)
+        response = [
+            "Alright #{username}, I'm ready to take notes.",
+            "Ok, sounds good. Ready when you are, #{username}!",
+            "I'm already on top of it, #{username}!",
+            "Alright, well then start typing #{username}!",
+            "For shizzle #{username}.",
+            "Uh.. Yeah. That's my job, #{username}.",
+            "Alright, #{username}. I've got you covered!",
+            "You bet, #{username}.",
+            "That I can do, #{username}.",
+            "If you say so, #{username}."
+          ].sample
+        response
+      end
+
+      def already_taking_notes_response(username)
+        response = [
+            "I've been taking notes, #{username}...",
+            "You already asked me to take notes #{username}...",
+            "Yeah #{username}, I've been doing that...",
+            "Uh huh, that's what I'm doing #{username}..."
+          ].sample
+        response
+      end
+
+      def not_taking_notes_response(username)
+        response = [
+            "You haven't told me to `take notes` yet #{username}...",
+            "You never asked me to take notes #{username}...",
+            "Sorry #{username}. You didn't ask me to, so I haven't been taking notes...",
+            "Ahh shoot, you sould have told me to `take notes` #{username}...",
+            "Ahh dang, you sould have told me to `start taking notes` #{username}..."
+          ].sample
+        response
+      end
+
+      def stopped_taking_notes_response(username)
+        response = [
+            "Ok, cool. I've compiled your notes and sent them out!",
+            "Right on, I'll send out the notes in a jiffy!",
+            "Alright, I'll get your notes put together and sent out right away!",
+            "Awesome, you'll see the notes in your email shortly."
+          ].sample
+        response
       end
 
     end
