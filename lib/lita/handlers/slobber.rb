b20efe282eafc9c16a8b2463323ad70160f25819
Chris Mikelson <chris.mikelson@640labs.com>
2015-10-02 21:40:42 -0500

    appends not truncate

--- lib/lita/handlers/slobber.rb
+++ lib/lita/handlers/slobber.rb
@@ -66,7 +66,7 @@ module Lita
         channel = get_channel(response)
         # unless is_taking_notes(channel) 
           FileUtils.mkdir_p("tmp/#{channel.id}") unless Dir.exists?("tmp/#{channel.id}")         
-          File.open("tmp/#{channel.id}/notes_session.log", 'w+') do |f|
+          File.open("tmp/#{channel.id}/notes_session.log", 'a') do |f|
             f.puts "[#{Time.now.to_i}] - [#{response.user.name}: #{response.message.body}"
           end
         # end
